9477c63c12cc8263fadd1c43ccb2272c66d54404
jatin foujdar <jatin321001@gmail.com>
2023-01-24 19:27:04 +0530

    bug fixed

--- app.js
+++ app.js
@@ -1,33 +1,48 @@
 import React from "react";
-import  ReactDOM  from "react-dom/client";
+import ReactDOM from "react-dom/client";
 
-const Title = ()=>(
-    <h1 id="title" key="h2">Food villa</h1>
+const Title = () => (
+<a href="/">
+ <img className="logo" alt = "logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Food_Network_Logo.svg/1200px-Food_Network_Logo.svg.png"/>
+ </a>
+ );
 
-)
-
-const HeaderComponents =()=>{
+// Composing Comopnentss
+const HeaderComponent = () => {
+  return (
+    <div className="header">
+      <Title />
+      <div className="nav-items">
+        <ul>
+            <li>Home </li>
+            <li>About us</li>
+            <li>Contact</li>
+            <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+const Body=()=>{
     return(
-        <div>
-            <Title/>
-  <div className="nav-item">
-    <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact Us</li>
-        <li>Cart</li>
-    </ul>
-  </div>
-        </div>
+        <h4>body</h4>
+    )
+}
+const Footer=()=>{
+    return(
+        <h4>Footer</h4>
     )
 }
 
-const AppLayout = ()=>{
+const AppLayout =() =>{
     return(
-{}
+  <>
+      <Header />
+      <Body />
+      <Footer/>
+  </>
     )
 }
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
-
-root.render(<HeaderComponents />)
+root.render(<HeaderComponent />);
