ceb2e8474e993ff8680952b3b7282b4116d305a2
jatin foujdar <jatin321001@gmail.com>
2023-01-27 22:55:28 +0530

    search filter commit

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,16 +1,40 @@
+import { useState } from "react";
 import { restaurantList } from "../config";
 import RestrauntCard from "./RestrauntCard";
 
+function filterData(searchTxt,restaurants){
+     const filterData = restaurants.filter((restaurant)=>
+        restaurant.data.name.includes(searchTxt)
+
+     )
+     return filterData;
+}
 
 const Body=()=>{
+   const [searchTxt,setSearchTxt] = useState("");
+   const [restaurants,setRestaurants]= useState(restaurantList);
     return(
+        <>
+        <div className="search container">
+          <input type="text" className="search-input" placeholder="Search" value={searchTxt}
+          onChange={(e)=> setSearchTxt(e.target.value)}></input>
+        <button className="search-btn"
+        onClick={()=>{
+            //filter the data
+           const data= filterData(searchTxt,restaurants);
+           //updating the data
+           setRestaurants(data);
+        }}>Search 
+        </button>
+        </div>
         <div className="restrauntCards">
             {
-                restaurantList.map((restaurant)=>{
+                restaurants.map((restaurant)=>{
                     return   <RestrauntCard {...restaurant.data} key={restaurant.data.id}/>
                 })
             }
         </div>
+        </>
     )
 }
 
