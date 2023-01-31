523f882bd504ea28d5c0c0b7c7361a5dc0758deb
jatin foujdar <jatin321001@gmail.com>
2023-02-01 00:21:17 +0530

    searching from filter commit

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -3,12 +3,15 @@ import React from 'react'
 const Shimmer = () => {
   return (
     <div class="container">
-    <article>
-        <div class="line"></div>
-        <div class="line"></div>
-        <div class="line"></div>
-        <div class="shimmer"></div>
-    </article>
+     {Array(5).fill("").map((e)=>(
+       <article>
+       <div class="line"></div>
+       <div class="line"></div>
+       <div class="line"></div>
+       <div class="shimmer"></div>
+   </article>
+     ))}
+   
 </div>
   )
 }
