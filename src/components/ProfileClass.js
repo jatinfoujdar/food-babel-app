85ab3d217cec819271e43143932dcf9c117e78dc
jatin foujdar <jatin321001@gmail.com>
2023-02-04 02:08:45 +0530

    class component api call commit

--- src/components/ProfileClass.js
+++ src/components/ProfileClass.js
@@ -6,18 +6,23 @@ class Profile extends React.Component{
     
       this.state = {
         count: 0,
-       
-      };     
+        };  
+        console.log("constructor")   
+    }
+    componentDidMount(){
+        //Api call
+        console.log("component did mount?");
     }
 render(){
     const {count} = this.state;
+    console.log("render");
   return(
   <div>
   <h1>Profile class components</h1>
   <h2>name:{this.props.name}</h2> 
   <h2>count:{count}</h2> 
   <button onClick={()=>{this.setState({
-    count: 1,
+    count: 1, 
   })
   }}>Setcount</button>
   </div>
