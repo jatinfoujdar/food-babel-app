0db6f1949b04e18850f82cde02696ad9a8942925
jatin foujdar <jatin321001@gmail.com>
2023-02-09 10:21:43 +0530

    error solved commit

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -2,17 +2,17 @@ import React from 'react'
 
 const Shimmer = () => {
   return (
-    <div className="container">
+    <div className='flex p-40 '>
      {Array(5).fill("").map((e)=>(
       
-       <div class="flex-1 space-y-6 py-1">
+       <div class="flex-1 space-y-6 py-1 border border-blue-300 shadow rounded-md p-4 max-w-sm w-full mx-auto ">
          <div class="h-2 bg-slate-200 rounded"></div>
          <div class="space-y-3">
            <div class="grid grid-cols-3 gap-4">
-             <div class="h-2 bg-slate-200 rounded col-span-2"></div>
-             <div class="h-2 bg-slate-200 rounded col-span-1"></div>
+             <div class="h-10 bg-slate-200 rounded col-span-2"></div>
+             <div class="h-10 bg-slate-200 rounded col-span-1"></div>
            </div>
-           <div class="h-2 bg-slate-200 rounded"></div>
+           <div class="h-10 bg-slate-200 rounded"></div>
            </div>
        </div>
 
