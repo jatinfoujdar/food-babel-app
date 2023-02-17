76251941a8b7bff172c8894f706e1f78cc54796a
jatin foujdar <jatin321001@gmail.com>
2023-02-17 19:41:39 +0530

    changes in instamart commit

--- src/components/Instamart.js
+++ src/components/Instamart.js
@@ -4,57 +4,46 @@ const Section = ({title, description, isVisible,setIsVisible})=>{
   return <div className= 'border border-black p-2 m-2 '>
      <h3 className='font-bold text-xl
      '>{title}</h3>
-     {isVisible ?(
+
+<button onClick={() => setIsVisible(!isVisible)}>
+        {isVisible ? "Show" : "Hide"}
+      </button>
+      {isVisible && <p>{description}</p>}
+  </div>
+
+
+     {/* {isVisible ?(
        <button onClick={()=>setIsVisible(false)} className='cursor-pointer underline'>Hide</button>
          
      )
            :
      (      
       <button onClick={()=>setIsVisible(true)} className='cursor-pointer underline'>Show</button>
-     )}
+     )} */}
     
-   
-     {isVisible && <p>{description}</p>}
-  </div>
 }
 
 
 const Instamart = () => {
-  const [sectionConfig,setSectionConfig]=useState({
-    showAbout:false,
-    showTeam: false,
-    showCareer:false,
-  })
+  const [visibleSection,setVisibleSection]=useState("about")
   return (
     <div >
       <h1 className='text-3xl p-2 m-2 font-bold'>Instamart</h1>
         <Section title={"About Instamart"}
         description={"this is the about section of Instamart"} 
-        isVisible={sectionConfig.showAbout}
-        setIsVisible={()=>setSectionConfig({
-          showAbout:true,
-          showTeam: false,
-          showCareer:false,
-        })}
+        isVisible={visibleSection == "about"}
+        setIsVisible={()=>setVisibleSection("about")}
         />
 
 <Section title={"Team Instamart"}
         description={"this is the about section of Team"}
-        isVisible={sectionConfig.showCareer} 
-        setIsVisible={()=>setSectionConfig({
-          showAbout:false,
-          showTeam: true,
-          showCareer:false,
-        })}/>
+        isVisible={visibleSection == "Team"} 
+        setIsVisible={()=>setVisibleSection("Team")}/>
 
 <Section title={"Careers "}
         description={"this is the about section of Career"} 
-        isVisible={sectionConfig.showTeam}
-        setIsVisible={()=>setSectionConfig({
-          showAbout:false,
-          showTeam: false,
-          showCareer:true,
-        })}/>    
+        isVisible={visibleSection == "careers"}
+        setIsVisible={()=>setVisibleSection("careers")}/>    
     </div>
   )
 }
